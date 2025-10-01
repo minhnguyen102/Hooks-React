c1f52b1534b2144810a2b8d42638ee1c3d2987e3
Minh Nguyen <minhkhac1002@gmail.com>
2025-10-01 15:10:09 +0700

    Example for one-way binding

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import { useState } from "react";
 
-const gifts = [
-    "Iphone 17 Promax",
-    "Honda CRV",
-    "Voucher discount 40%"
-  ]
-let count = 0
 function App() {
-  const [gift, setGift] = useState();
-  const handleClick = () => {
-    if(count < 3){
-      count++
-      console.log(count);
-      setGift(gifts[Math.floor(Math.random() * gifts.length)])
-    }else{
-      alert("Hết lượt thay đổi")
-    }
+  const [name, setName] = useState()
+  
+  console.log(name)
+  const handleSubmit = () => {
+    console.log({name})
   }
-
   return (
     <div className="App" style={{padding: 20}}>
-      <h1>{gift || "Chưa có phần thưởng" }</h1>
-      <button onClick={handleClick}>Lấy thưởng</button>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={(e) => setName("Nguyen Van B")}>Change</button>
+      <button onClick={handleSubmit}>Submit</button>
+
     </div>
   );
 }
