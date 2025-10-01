01450474f0bc57fadc5921d925ae4d276fccf10c
Minh Nguyen <minhkhac1002@gmail.com>
2025-10-01 15:21:21 +0700

    Example 2 for two-way binding

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import { useState } from "react";
 
+const courses = [
+  {
+    id: 1,
+    name: "Javascript"
+  },
+  {
+    id: 2,
+    name: "HTML, CSS"
+  },
+  {
+    id: 3,
+    name: "React"
+  },
+]
+
+
+
+
+
 function App() {
-  const [name, setName] = useState()
-  
-  console.log(name)
-  const handleSubmit = () => {
-    console.log({name})
+  const [checked, setChecked] = useState(1)
+
+  const handleChange = (id) => {
+    //setChecked
+    setChecked(id)
   }
+
+  const handleSubmit = () => {
+    // Call API
+    console.log({
+      id: checked
+    })
+  } 
+  
   return (
     <div className="App" style={{padding: 20}}>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button onClick={(e) => setName("Nguyen Van B")}>Change</button>
+      {courses.map(course => {
+        return (
+          <div key={course.id}> 
+            <input
+             type="radio"
+             checked={checked === course.id}
+             onChange={() => handleChange(course.id)}
+            />
+            {course.name}
+          </div>
+        )
+      })}
       <button onClick={handleSubmit}>Submit</button>
-
     </div>
   );
 }
