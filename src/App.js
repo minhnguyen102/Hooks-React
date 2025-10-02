03d5f6a09687fd79530c4929813a1875e3dcdddf
Minh Nguyen <minhkhac1002@gmail.com>
2025-10-02 17:06:45 +0700

    Todo list use useState

--- src/App.js
+++ src/App.js
@@ -1,57 +1,37 @@
 import { useState } from "react";
 
-const courses = [
-  {
-    id: 1,
-    name: "Javascript"
-  },
-  {
-    id: 2,
-    name: "HTML, CSS"
-  },
-  {
-    id: 3,
-    name: "React"
-  },
-]
-
 function App() {
-  const [checked, setChecked] = useState([])
-
-  const handleChange = (id) => {
-    //setChecked
-    setChecked(pre => {
-      if(checked.includes(id)){
-        // uncheck
-        return (checked.filter(item => item !== id))
-      }else{
-        return [
-          ...checked,
-          id
-        ]
-      }
-    })
-  }
+  const [job, setJob] = useState("")
+  const [jobs, setJobs] = useState(JSON.parse(localStorage.getItem("jobs")) ?? [])
 
   const handleSubmit = () => {
-    // Call API
-    console.log(checked)
-  } 
-  
+    setJobs(pre => {
+      const jobLocalStorage = [...jobs, job]
+      localStorage.setItem("jobs", JSON.stringify(jobLocalStorage))
+      // console.log(jobLocalStorage) // dòng này lại được in ra sau 
+      return jobLocalStorage
+    })
+    setJob("")
+
+    // console.log(jobs) // có mội trình xử lí bên dưới (Dòng này được in trước => trình xử lí chưa setJob vội)
+    // Cần hiểu rõ luồng hoạt động của dòng 16 và dòng 11
+  }
+
   return (
     <div className="App" style={{padding: 20}}>
-      {courses.map(course => {
-        return (
-          <div key={course.id}> 
-            <input
-             type="checkbox"
-             onChange={() => handleChange(course.id)}
-            />
-            {course.name}
-          </div>
-        )
-      })}
-      <button onClick={handleSubmit}>Submit</button>
+      <input 
+        value={job} 
+        onChange={e => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+
+      <ul>
+        {jobs.map((job, item) => {
+          return (
+            <li key={item}>{job}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
